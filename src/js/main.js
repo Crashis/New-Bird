e865195cb4561005113f3995dd787db58d6396be
PetrSturc <sturcp@seznam.cz>
2026-05-16 16:36:03 +0200

    refaktor pro pages

--- src/js/main.js
+++ src/js/main.js
@@ -1,34 +1,34 @@
 const scriptChunks = [
-  "./core/storage.js",
-  "./core/utils.js",
-  "./ui/dom.js",
-  "./ui/menu.js",
-  "./features/audio.js",
-  "./features/skinsData.js",
-  "./core/state.js",
-  "./features/achievements.js",
-  "./ui/toast.js",
-  "./core/constants.js",
-  "./features/currencies.js",
-  "./features/skins.js",
-  "./ui/panels.js",
-  "./features/settings.js",
-  "./features/cheatCodes.js",
-  "./features/shop.js",
-  "./game/eventPhase.js",
-  "./game/score.js",
-  "./game/obstacles.js",
-  "./game/collision.js",
-  "./game/player.js",
-  "./game/gameLoop.js",
-  "./game/input.js",
-  "./ui/fullscreen.js"
+  "./src/js/core/storage.js",
+  "./src/js/core/utils.js",
+  "./src/js/ui/dom.js",
+  "./src/js/ui/menu.js",
+  "./src/js/features/audio.js",
+  "./src/js/features/skinsData.js",
+  "./src/js/core/state.js",
+  "./src/js/features/achievements.js",
+  "./src/js/ui/toast.js",
+  "./src/js/core/constants.js",
+  "./src/js/features/currencies.js",
+  "./src/js/features/skins.js",
+  "./src/js/ui/panels.js",
+  "./src/js/features/settings.js",
+  "./src/js/features/cheatCodes.js",
+  "./src/js/features/shop.js",
+  "./src/js/game/eventPhase.js",
+  "./src/js/game/score.js",
+  "./src/js/game/obstacles.js",
+  "./src/js/game/collision.js",
+  "./src/js/game/player.js",
+  "./src/js/game/gameLoop.js",
+  "./src/js/game/input.js",
+  "./src/js/ui/fullscreen.js"
 ];
 
 function loadClassicScript(src) {
   return new Promise((resolve, reject) => {
     const script = document.createElement('script');
-    script.src = new URL(src, import.meta.url).href;
+    script.setAttribute('src', src);
     script.async = false;
     script.onload = resolve;
     script.onerror = () => reject(new Error(`Failed to load game script: ${src}`));
