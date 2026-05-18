99012e61d8f7aec8f9b46a330e97a4984b30f569
PetrSturc <sturcp@seznam.cz>
2026-05-18 19:15:36 +0200

    feat(cloud-save): add progress key registry to NWStorage

--- src/js/core/storage.js
+++ src/js/core/storage.js
@@ -39,3 +39,51 @@ window.NWStorage.loadNumber = function loadNumber(key, fallback = 0) {
 window.NWStorage.saveNumber = function saveNumber(key, value) {
   try { localStorage.setItem(key, String(value)); } catch (e) {}
 };
+
+window.NWStorage.PROGRESS_KEYS = Object.freeze({
+  // Currencies
+  BEST: 'nw_flappy_best',
+  YANG: 'nw_flappy_yang',
+  WALLETS: 'nw_flappy_wallets',
+  DRAGON_COINS: 'nw_flappy_dragon_coins',
+  ERR_CUBES: 'nw_flappy_err_cubes',
+  // Upgrades
+  UPGRADE_SHIELD_START: 'nw_flappy_upgrade_shield_start',
+  UPGRADE_INVINCIBILITY: 'nw_flappy_upgrade_invincibility',
+  UPGRADE_DOUBLE_YANG: 'nw_flappy_upgrade_double_yang',
+  UPGRADE_CROWN_BONUS: 'nw_flappy_upgrade_crown_bonus',
+  UPGRADE_MAX_SHIELDS_2: 'nw_flappy_upgrade_max_shields_2',
+  PLAYER_UPGRADES: 'nw_flappy_player_upgrades',
+  IMMORTALITY_USES: 'nw_flappy_immortality_uses',
+  // Skins
+  SELECTED_SKIN: 'nw_flappy_selected_skin',
+  UNLOCKED_SKINS: 'nw_flappy_unlocked_skins',
+  // Achievements
+  ACHIEVEMENTS: 'nw_flappy_achievements',
+  // Heirlooms
+  HEIRLOOM_ROCKET_PURCHASED: 'heirloomRocketPurchased',
+  HEIRLOOM_ROCKET_EQUIPPED: 'heirloomRocketEquipped',
+  HEIRLOOM_POTION_PURCHASED: 'heirloomPotionPurchased',
+  HEIRLOOM_POTION_EQUIPPED: 'heirloomPotionEquipped',
+  HEIRLOOM_GODIAS_PURCHASED: 'heirloomGodiasPurchased',
+  HEIRLOOM_GODIAS_EQUIPPED: 'heirloomGodiasEquipped',
+  HEIRLOOM_CONCERT_PURCHASED: 'heirloomConcertPurchased',
+  HEIRLOOM_PAYSAFE_PURCHASED: 'heirloomPaysafePurchased',
+  HEIRLOOM_NESCHOPENKA_PURCHASED: 'heirloomNeschopenkaPurchased',
+  // Boss / dungeon
+  BEZOS_BOSS_TICKET: 'bezosBossTicketUnlocked',
+  BEZOS_BOSS_LAST_WIN: 'bezosBossLastWinDate',
+  BEZOS_BOSS_BONUS_USED: 'bezosBossBonusUsedDate',
+  // Daily cooldowns
+  DRAGON_EGG_DATE: 'dragonEggDailyDate',
+  DRAGON_EGG_USES: 'dragonEggDailyUses',
+  DRAGON_EGG_STATE: 'dragonEggState',
+  BLACKSMITH_DATE: 'blacksmithChallengePlayDate',
+  BLACKSMITH_PLAYS: 'blacksmithChallengePlaysToday',
+  THREE_CHESTS_DATE: 'threeChestsLastPlayedDate',
+  WHEEL_DATE: 'wheelOfFortuneLastPlayedDate',
+  // Nickname
+  PLAYER_NAME: 'nw_player_name'
+});
+
+window.NWStorage.PROGRESS_KEY_LIST = Object.values(window.NWStorage.PROGRESS_KEYS);
